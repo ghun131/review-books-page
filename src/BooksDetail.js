f5625f5461243aefab9477259969472f83256343
hungHoangDang <33661599+hungHoangDang@users.noreply.github.com>
2018-04-25 21:18:44 +0700

    change a lot

--- src/BooksDetail.js
+++ src/BooksDetail.js
@@ -1,19 +1,67 @@
 import React, { Component } from "react";
-import { Image } from "react-bootstrap";
-import './BookDetail.css'
+import { Button, Image } from "react-bootstrap";
+import "./BookDetail.css";
+import ReactModal from "react-modal";
+
+
+const customStyles = {
+  content : {
+    top : '50%',
+    left : '50%',
+    width: '50%',
+    height: '80%',
+    right : 'auto',
+    bottom : 'auto',
+    marginRight : '-50%',
+    transform : 'translate(-50%, -50%)',
+    textAlign : 'center'
+  }
+};
+
+ReactModal.setAppElement('#root')
 
 export default class BooksDetail extends Component {
+  constructor() {
+    super();
+    this.state = { showModal: false };
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal() {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal() {
+    this.setState({ showModal: false });
+  }
   render() {
     const book = this.props.book;
     const bookImageUrl = process.env.PUBLIC_URL + `/pictures/${book.picture}`;
+    //const bookImageUrl = "http://via.placeholder.com/200x300"
     return (
       <div className="book-detail">
-        <h2>{book.title}</h2>
-        <Image className="center-block" src={bookImageUrl} responsive />
-        <ul className="list-unstyled">
-          <li><em>{book.author}</em></li>
-          <li>{book.review}</li>
-        </ul>
+        <Image
+          onClick={this.handleOpenModal}
+          className="center-block"
+          src={bookImageUrl}
+          responsive
+        />
+        <ReactModal
+          isOpen={this.state.showModal}
+          style={customStyles}
+          contentLabel="Author and Review"
+        >
+          <Image className="center-block" src={bookImageUrl} responsive />
+          <ul className="list-unstyled">
+            <li>
+              <em>{book.author}</em>
+            </li>
+            <li>{book.review}</li>
+          </ul>
+          <Button onClick={this.handleCloseModal}> close </Button>
+        </ReactModal>
       </div>
     );
   }
--- src/BooksList.js
+++ src/BooksList.js
@@ -1,37 +1,22 @@
 import React, { Component } from "react";
 import BooksDetail from "./BooksDetail";
 import api from "./api";
-import { Grid, Row, Col, Clearfix } from "react-bootstrap";
+import { Grid, Row, Col } from "react-bootstrap";
 
 export default class BooksList extends Component {
   render() {
-    const bookInfo = api.map(book => {
-      return <BooksDetail key={book.title} book={book} />;
-    });
-
-    /*let firstCol = [];
-    let secondCol = [];
-    const smallerArr = (col, num) => {
-      for (let i = num; i < bookInfo.length; i += 2) {
-        col.push(bookInfo[i]);
-      }
-      return col;
-    };
-
-    firstCol = smallerArr(firstCol, 0);
-    secondCol = smallerArr(secondCol, 1);
-*/
+    const bookInfo = api.map(book => (
+      <Col xs={12} sm={6} md={4} key={book.title}>
+        <BooksDetail key={book.title} book={book} />
+      </Col>
+    ));
 
     return (
-      <span>
+      <div>
         <Grid>
-          <Row className="show-grid">
-            <Col xs={12} sm={6} md={6}>
-              <ul>{bookInfo}</ul>
-            </Col>
-          </Row>
+          <Row className="show-grid">{bookInfo}</Row>
         </Grid>
-      </span>
+      </div>
     );
   }
 }
